946717d77ca6bc5eead396b29b11941847f539bc
임준혁 <junhyuk1027@naver.com>
2024-07-16 13:44:40 +0900

    Feat: 최초 커밋
    gitignore 추가
    
    의존성 추가

---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/LingolandApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.a603.lingoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingolandApplication.class, args);
+	}
+
+}
---
+++ lingoland-backend/src/test/java/com/ssafy/a603/lingoland/LingolandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a603.lingoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
