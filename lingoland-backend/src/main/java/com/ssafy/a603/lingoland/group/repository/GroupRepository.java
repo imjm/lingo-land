93a7e34c7c38ad9b794ac351ada4849d61ea5b45
이진석 <tjrl1225@naver.com>
2024-07-18 15:50:54 +0900

    Feat: 그룹 엔티티 및 레포지토리 구현 작성된 ERD를 베이스로 Group과 GroupMember 연결테이블, 복합키를 위한 GroupMemberId 엔티티 클래스를 구현했습니다. Group에 대한 영속성을 사용하기 위해 Repository를 구현했습니다. 티켓: S11P12A603-249

---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/group/repository/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.a603.lingoland.group.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.a603.lingoland.group.entity.Group;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+}
