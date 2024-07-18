d390f693b49d9d2930528d83e60e533864c4c65b
이진석 <tjrl1225@naver.com>
2024-07-18 16:11:22 +0900

    Test: 그룹 엔티티 테스트 작성 Group, GroupMember Entity에 대한 단위 테스트를 Diffblue cover를 통해 구현했습니다. 티켓: S11P12A603-249

---
+++ lingoland-backend/src/test/java/com/ssafy/a603/lingoland/group/entity/GroupMemberDiffblueTest.java
@@ -0,0 +1,36 @@
+package com.ssafy.a603.lingoland.group.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.ssafy.a603.lingoland.member.Member;
+
+class GroupMemberDiffblueTest {
+	/**
+	 * Method under test:
+	 * {@link GroupMember#GroupMember(GroupMemberId, String, Group, Member)}
+	 */
+	@Test
+	void testNewGroupMember() {
+		// Arrange
+		GroupMemberId id = new GroupMemberId();
+
+		Group group = new Group();
+		group.setGroupImagePath("Path");
+		Member member = new Member();
+
+		// Act
+		GroupMember actualGroupMember = new GroupMember(id, "The characteristics of someone or something", group,
+			member);
+
+		// Assert
+		assertEquals("The characteristics of someone or something", actualGroupMember.getDescription());
+		assertNull(actualGroupMember.getCreatedAt());
+		assertNull(actualGroupMember.getDeletedAt());
+		assertFalse(actualGroupMember.isDeleted());
+		assertSame(group, actualGroupMember.getGroup());
+		assertSame(id, actualGroupMember.getId());
+		assertSame(member, actualGroupMember.getMember());
+	}
+}
