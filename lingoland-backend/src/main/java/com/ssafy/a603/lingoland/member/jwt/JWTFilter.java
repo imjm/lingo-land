1d77d5c8dc62cf55143cea9e7c92efde4ad83cf4
전정민 <junsk50@naver.com>
2024-07-19 14:32:50 +0900

    Feat: 토큰 생성 구현 access 토큰과 refresh 토큰을 통한 이중 토큰 구현했습니다. 티켓: S11P12A603-75

---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/jwt/JWTFilter.java
@@ -0,0 +1,67 @@
+package com.ssafy.a603.lingoland.member.jwt;
+
+import com.ssafy.a603.lingoland.member.CustomUserDetails;
+import com.ssafy.a603.lingoland.member.Member;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String accessToken = request.getHeader("access");
+
+        if(accessToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
+            // TODO 프론트랑 어떤 코드 줄지 상의
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String category = jwtUtil.getCategory(accessToken);
+
+        if(!category.equals("access")) {
+
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access expired");
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String loginId = jwtUtil.getLoginId(accessToken);
+
+        Member member = Member.builder()
+                .loginId(loginId)
+                .build();
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(member);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null);
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
+    }
+}
