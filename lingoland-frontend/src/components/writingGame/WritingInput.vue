1fc9e2c8790fd61ecb0f31740c582541bc035377
김상혁 <ksang0728@gmail.com>
2024-07-19 18:07:38 +0900

    Feat: writingGame 컴포넌트 제작 writingGame 컴포넌트 기본 틀 구현했습니다. 티켓: S11P12A603-248

---
+++ lingoland-frontend/src/components/writingGame/WritingInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SubmitButton from "../common/SubmitButton.vue";
+</script>
+
+<template>
+    <v-card width="800">
+        <div>몇 초 남았을까요?</div>
+        <v-textarea variant="outlined">
+            
+        </v-textarea>
+        <SubmitButton data="제출" />
+    </v-card>
+</template>
+
+<style scoped></style>
---
+++ lingoland-frontend/src/components/writingGame/WritingList.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+        지금까지 쓴 글입니다.
+</template>
+
+<style scoped>
+
+</style>
