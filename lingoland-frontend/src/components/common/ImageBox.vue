791951e877b244eb9f13622be8de8bd222d6f2e9
김상혁 <ksang0728@gmail.com>
2024-07-19 15:38:04 +0900

    Fix: impageBox 기본 컴포넌트 props 수정 imagebox 컴포넌트에서 props를 받을 수 있도록 수정했습니다. 티켓: S11P12A603-248

--- lingoland-frontend/src/components/common/ImageBox.vue
+++ lingoland-frontend/src/components/common/ImageBox.vue
@@ -6,7 +6,6 @@
  source: String, 이미지 소스
 */
 const props = defineProps({
-    aspectRatio: String,
     height: Number,
     width: Number,
     source: String,
@@ -18,7 +17,6 @@ const props = defineProps({
         <v-img
             :width="width"
             :height="height"
-            :aspect-ratio="aspectRatio"
             cover
             :src="source"
             class="rounded-circle"
