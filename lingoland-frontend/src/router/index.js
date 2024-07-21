1a2357da9f3a1a8d21739b66dbead434d5c73464
tony <ksang0728@gmail.com>
2024-07-22 07:31:29 +0900

    Feat: writingroom 컴포넌트 제작 writingroom 컴포넌트 중 writinginput을 수정했습니다. writinggameview를 수정했습니다. 티켓: S11P12A603-248

--- lingoland-frontend/src/router/index.js
+++ lingoland-frontend/src/router/index.js
@@ -3,28 +3,33 @@ import GameRoomView from "@/views/GameRoomView.vue";
 import SignUpView from "@/views/SignUpView.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
+import WritingGameView from "@/views/WritingGameView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
 
-  routes: [
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/rooms",
-      name: "gameRoom",
-      component: GameRoomView,
-    },
-
-    {
-      path: "/signup",
-      name: "signUp",
-      component: SignUpView,
-    },
-  ],
+    routes: [
+        {
+            path: "/login",
+            name: "login",
+            component: LoginView,
+        },
+        {
+            path: "/rooms",
+            name: "gameRoom",
+            component: GameRoomView,
+        },
+        {
+            path: "/signup",
+            name: "signUp",
+            component: SignUpView,
+        },
+        {
+            path: "/writingGame",
+            name: "writingGame",
+            component: WritingGameView,
+        },
+    ],
 });
 
 export default router;
