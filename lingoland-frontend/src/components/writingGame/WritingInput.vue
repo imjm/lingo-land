943463e6ec7862487206de1f96bbbf720f291047
tony <ksang0728@gmail.com>
2024-07-22 07:31:42 +0900

    Feat: writingroom 컴포넌트 제작 writingroom 컴포넌트 중 writinginput을 수정했습니다. writinggameview를 수정했습니다. 티켓: S11P12A603-248

--- lingoland-frontend/src/components/writingGame/WritingInput.vue
+++ lingoland-frontend/src/components/writingGame/WritingInput.vue
@@ -3,11 +3,9 @@ import SubmitButton from "../common/SubmitButton.vue";
 </script>
 
 <template>
-    <v-card width="800">
+    <v-card width="500">
         <div>몇 초 남았을까요?</div>
-        <v-textarea variant="outlined">
-            
-        </v-textarea>
+        <v-textarea variant="outlined"> </v-textarea>
         <SubmitButton data="제출" />
     </v-card>
 </template>
--- lingoland-frontend/src/views/WritingGameView.vue
+++ lingoland-frontend/src/views/WritingGameView.vue
@@ -1,13 +1,32 @@
 <script setup>
-
+import WritingInput from "@/components/writingGame/WritingInput.vue";
+import WritingList from "@/components/writingGame/WritingList.vue";
 </script>
 
 <template>
-    <div>
-        
-    </div>
+    <v-layout>
+        <v-main class="background-image d-flex flex-column align-center justify-center">
+            <v-row>
+                <div class="text-h3 text-white">
+                    다음 글을 읽고 이어질 이야기를 작성하세요
+                </div>
+            </v-row>
+
+            <v-row class="d-flex align-center justify-space-evenly">
+                <WritingList />
+                <WritingInput />
+            </v-row>
+        </v-main>
+    </v-layout>
 </template>
 
 <style scoped>
-
+.background-image {
+    background-image: url("src\\assets\\blackboard.png");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100%;
+}
 </style>
