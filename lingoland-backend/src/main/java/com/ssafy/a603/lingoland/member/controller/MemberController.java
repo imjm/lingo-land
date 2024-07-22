cc2b11949f627035542c309e39453abd9b716a02
전정민 <junsk50@naver.com>
2024-07-22 13:24:30 +0900

    Fix: api versioning 적용 api 버저닝을 적용했습니다. 티켓: S11P12A603-305

--- lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/controller/MemberController.java
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class MemberController {
 
     private final MemberService memberService;
--- lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/controller/ReissueController.java
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/controller/ReissueController.java
@@ -12,10 +12,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ReissueController {
 
     private final JWTUtil jwtUtil;
