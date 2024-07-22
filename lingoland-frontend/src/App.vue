4e0a99670137f5c203675716fbed83137238534e
김상혁 <ksang0728@gmail.com>
2024-07-22 15:26:32 +0900

    Fix: FE - refactoring 프론트엔드 코드 리팩토링을 했습니다. 티켓: S11P12A603-307

--- lingoland-frontend/src/App.vue
+++ lingoland-frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterView } from "vue-router";
-import TaleDetailView from "@/views/TalesDetailView.vue"
 </script>
 
 <template>
@@ -9,12 +8,12 @@ import TaleDetailView from "@/views/TalesDetailView.vue"
     <RouterLink :to="{ name: 'signUp' }"> signup</RouterLink> |
     <RouterLink :to="{ name: 'writingGame' }"> writingGame</RouterLink> |
     <RouterLink :to="{ name: 'writingGameResult' }">
-    <RouterLink :to="{ name: 'mainPage' }"> mainPage</RouterLink> |
-    <RouterLink :to="{ name: 'groups' }">groupjoin</RouterLink>|
-    <RouterLink :to="{ name: 'groupId' }">groupId</RouterLink>|
         writingGameResult</RouterLink
     >
-    |
+    | <RouterLink :to="{ name: 'mainPage' }"> mainPage</RouterLink> |
+    <RouterLink :to="{ name: 'groups' }">groupjoin</RouterLink> |
+    <RouterLink :to="{ name: 'groupId' }">groupId</RouterLink> |
+
     <v-app class="bg-amber-lighten-3">
         <RouterView></RouterView>
     </v-app>
