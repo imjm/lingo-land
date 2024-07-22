e11cd226057fdfdf95c2b831b1ad18a357f70cb6
Hungry404 <bdjg1029@gmail.com>
2024-07-22 13:00:56 +0900

    Feat: FE 그룹/가입/리스트 작성했던 grouplist를 바탕으로 groupjoinview를 완성하였습니다. searchinput 크기 조절같은 디자인 요소 조절이 조금 필요합니다. 티켓: S11P12A603-65

--- lingoland-frontend/src/views/GroupJoinView.vue
+++ lingoland-frontend/src/views/GroupJoinView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import GroupList from "@/components/group/GroupList.vue";
+import SearchInput from "@/components/common/SearchInput.vue";
 </script>
 
 <template>
+  <br />
   <main>
-    <GroupList />
+    <v-sheet rounded class="ma-10">
+      <h1 class="mx-10">그룹 목록</h1>
+      <SearchInput />
+      <GroupList />
+    </v-sheet>
   </main>
 </template>
