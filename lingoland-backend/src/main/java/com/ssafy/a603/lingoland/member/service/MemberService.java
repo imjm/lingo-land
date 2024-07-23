ade33cd2af4261ea3f0bc0608f047c576ad10fc5
전정민 <junsk50@naver.com>
2024-07-23 17:01:27 +0900

    fix: 컴플릭트에서 생긴 스크립트 수정

--- lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/service/MemberService.java
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/service/MemberService.java
@@ -64,7 +64,7 @@ public class MemberService implements UserDetailsService {
         Member member = getMember(customUserDetails.getUsername());
         return GetMemberInfoDto.builder()
                 .nickname(member.getNickname())
-                .profileImage(member.getProfile_image())
+                .profileImage(member.getProfileImage())
                 .experiencePoint(member.getExperiencePoint())
                 .build();
     }
@@ -78,54 +78,5 @@ public class MemberService implements UserDetailsService {
     private Member getMember(String loginId) {
         return memberRepository.findByLoginId(loginId).orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다"));
     }
-=======
-	@Transactional
-	public Member saveNewMember(SignUpRequest signUpRequest) {
-		Member member = Member.builder()
-			.loginId(signUpRequest.getLoginId())
-			.nickname(signUpRequest.getNickname())
-			.password(passwordEncoder.encode(signUpRequest.getPassword()))
-			.createdAt(LocalDateTime.now())
-			.rank("d")
-			.build();
-		return memberRepository.save(member);
-	}
 
-	@Override
-	public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다"));
-		return new CustomUserDetails(member);
-	}
-
-	@Transactional
-	public void addRefreshToken(String loginId, String refresh) {
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다"));
-		member.updateRefreshToken(refresh);
-	}
-
-	@Transactional
-	public void deleteRefreshToken(String loginId) {
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다"));
-		member.updateRefreshToken(null);
-	}
-
-	public Boolean checkExistRefreshToken(String loginId) {
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다"));
-		return member.getRefreshToken() != null;
-	}
-
-	public MemberInfoDto getMemberInfo(CustomUserDetails customUserDetails) {
-		Member member = memberRepository.findByLoginId(customUserDetails.getUsername())
-			.orElseThrow(() -> new NoSuchElementException("존재하지 않은 유저입니다."));
-		return MemberInfoDto.builder()
-			.nickname(member.getNickname())
-			.profileImage(member.getProfileImage())
-			.experiencePoint(member.getExperiencePoint())
-			.build();
-	}
->>>>>>> lingoland-backend/src/main/java/com/ssafy/a603/lingoland/member/service/MemberService.java
 }
