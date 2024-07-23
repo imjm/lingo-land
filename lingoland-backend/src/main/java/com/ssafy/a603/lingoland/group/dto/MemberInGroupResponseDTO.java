bda3fe817bb57c4a986f04e1cc2e3fc88a55f384
이진석 <tjrl1225@naver.com>
2024-07-23 15:27:25 +0900

    Feat: 그룹원 조회를 위한 QueryDsl 적용 그룹 검색에 동적 쿼리를 적용하기 위해 QueryDsl을 적용합니다. Repository를 Querydsl을 사용하도록 수정합니다. 티켓: S11P12A603-296

---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/group/dto/MemberInGroupResponseDTO.java
@@ -0,0 +1,4 @@
+package com.ssafy.a603.lingoland.group.dto;
+
+public record MemberInGroupResponseDTO(String nickname, String profileImage, String description, boolean isLeader) {
+}
---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/group/repository/GroupCustomRepositoryImpl.java
@@ -0,0 +1,64 @@
+package com.ssafy.a603.lingoland.group.repository;
+
+import static com.ssafy.a603.lingoland.group.entity.QGroup.*;
+import static com.ssafy.a603.lingoland.group.entity.QGroupMember.*;
+import static com.ssafy.a603.lingoland.member.entity.QMember.*;
+
+import java.util.List;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.util.StringUtils;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.a603.lingoland.group.dto.MemberInGroupResponseDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GroupCustomRepositoryImpl implements GroupCustomRepository {
+	private final JPAQueryFactory queryFactory;
+
+	public List<MemberInGroupResponseDTO> findAllMembresInGroup(int groupId, String keyword) {
+		List<MemberInGroupResponseDTO> members = queryFactory.select(
+				Projections.constructor(MemberInGroupResponseDTO.class,
+					member.nickname,
+					member.profileImage,
+					groupMember.description,
+					member.id.eq(group.leader.id).as("isLeader")
+				))
+			.from(member)
+			.join(groupMember).on(member.id.eq(groupMember.id.memberId))
+			.join(group).on(groupMember.id.groupId.eq(group.id))
+			.where(
+				targetGroup(groupId),
+				containsNickname(keyword)
+			)
+			.fetch();
+		return moveLeaderToTop(members);
+	}
+
+	private List<MemberInGroupResponseDTO> moveLeaderToTop(List<MemberInGroupResponseDTO> members) {
+		int leaderIndex = -1;
+
+		for (int i = 0; i < members.size(); i++) {
+			if (members.get(i).isLeader()) {
+				leaderIndex = i;
+				break;
+			}
+		}
+		
+		if (leaderIndex != -1 && leaderIndex != 0) {
+			MemberInGroupResponseDTO leader = members.remove(leaderIndex);
+			members.add(0, leader);
+		}
+		return members;
+	}
+
+	private BooleanExpression targetGroup(int groupId) {
+		return groupMember.group.id.eq(groupId);
+	}
+
+	private BooleanExpression containsNickname(String keyword) {
+		return StringUtils.isNullOrEmpty(keyword) ? null : member.nickname.containsIgnoreCase(keyword);
+	}
+}
