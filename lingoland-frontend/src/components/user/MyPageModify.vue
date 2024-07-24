fa907e109b78211259400158d5db48d32e31114a
김상혁 <ksang0728@gmail.com>
2024-07-24 14:28:44 +0900

    Feat: 마이페이지 수정 뷰 수정 - 마이페이지 수정 화면 UI 수정 - 닉네임 변경 - 현재 비밀번호 확인 후 비밀번호 변경 페이지로 이동 티켓: S11P12A603-321

--- lingoland-frontend/src/components/user/MyPageModify.vue
+++ lingoland-frontend/src/components/user/MyPageModify.vue
@@ -1,19 +1,22 @@
 <script setup>
-import imageSource from '@/assets/sampleImg.jpg';
+import imageSource from "@/assets/sampleImg.jpg";
 import GenericInput from "../common/GenericInput.vue";
 import ImageBox from "../common/ImageBox.vue";
 import SubmitButton from "../common/SubmitButton.vue";
-
+import GenericButton from "../common/GenericButton.vue";
 
 function modify() {
-    console.log("수정해");
+    console.log("현재 비밀번호가 맞는지 확인 후에 비밀번호 변경 페이지로 이동");
 }
 </script>
 
 <template>
     <v-main class="d-flex align-center justify-center">
-        <v-card width="1200">
-            <!-- 하나의 행을 만듬 -->
+        <v-card
+            width="1200"
+            height="500"
+            class="d-flex align-center justify-center"
+        >
             <v-row>
                 <v-col cols="6" class="d-flex align-center justify-center">
                     <ImageBox :source="imageSource" />
@@ -21,24 +24,34 @@ function modify() {
 
                 <v-col cols="6">
                     <div class="ma-10">
-                        <GenericInput type="text" data="이름" id="userName" />
-
-                        <GenericInput type="text" data="아이디" id="userId" />
+                        <v-row class="d-flex align-center justify-center">
+                            <v-col cols="10">
+                                <GenericInput
+                                    type="text"
+                                    data="닉네임"
+                                    id="userId"
+                                    hint="변경할 닉네임을 입력하세요"
+                                />
+                            </v-col>
+                            <v-col cols="2" class="px-0">
+                                <GenericButton
+                                    data="변경하기"
+                                    id="modifyUserNickName"
+                                    height="56"
+                                />
+                            </v-col>
+                        </v-row>
 
                         <GenericInput
                             type="password"
-                            data="비밀번호 변경"
+                            data="현재 비밀번호 "
                             id="userPassword"
-                        />
-                        <GenericInput
-                            type="password"
-                            data="비밀번호 확인"
-                            id="userPasswordCheck"
+                            hint="현재 비밀번호를 입력하세요"
                         />
 
                         <SubmitButton
                             id="signUp"
-                            data="수정하기"
+                            data="비밀번호 변경하러 가기"
                             width="100%"
                             @click-event="modify"
                         />
