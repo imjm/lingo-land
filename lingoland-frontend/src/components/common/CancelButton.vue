a26abe25137bed607fe6505dfb416994833b4582
김상혁 <ksang0728@gmail.com>
2024-07-24 14:55:58 +0900

    Feat: common 컴포넌트 추가 취소 버튼 - common 컴포넌트에 취소 버튼을 추가했습니다. 티켓: S11P12A603-321

---
+++ lingoland-frontend/src/components/common/CancelButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from "vue";
+/*
+ id: 컴포넌트 id
+ type: button 컴포넌트 type 지정
+ data: button 컴포넌트에 지정할 이름
+  color: 색상
+*/
+const props = defineProps({
+    id: String,
+    type: String,
+    data: String,
+    height: String,
+});
+</script>
+
+<template>
+    <v-btn class="my-5" :id="id" variant="outlined":height="height" @click="$emit('clickEvent')">{{ data }}</v-btn>
+</template>
+
+<style scoped></style>
