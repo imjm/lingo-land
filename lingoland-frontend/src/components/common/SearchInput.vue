ed85c197387f6fcd4188bb4a1a157693435ff65b
김상혁 <ksang0728@gmail.com>
2024-07-25 13:49:15 +0900

    Feat: 하위컴포넌트 데이터를 가져올 수 있도록 model 설정 - 하위 컴포넌트 데이터를 가져올 수 있도록 defineModel을 추가했습니다. 티켓: S11P12A603-324

--- lingoland-frontend/src/components/common/SearchInput.vue
+++ lingoland-frontend/src/components/common/SearchInput.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { defineProps } from "vue";
+import { defineModel, defineProps } from "vue";
 /*
  id: 컴포넌트 id
  type: input 컴포넌트 type 지정
  data: input 컴포넌트 위에 지정할 이름 
 */
+const model = defineModel();
+
 const props = defineProps({
     id: String,
     type: String,
@@ -14,7 +16,7 @@ const props = defineProps({
 
 <template>
     <div>
-        <v-text-field :type="type" :id="id"></v-text-field>
+        <v-text-field v-model="model" :type="type" :id="id"></v-text-field>
     </div>
 </template>
 
