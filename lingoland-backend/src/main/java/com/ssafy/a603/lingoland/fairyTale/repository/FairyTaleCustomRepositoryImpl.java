184eda4d7d8b65a497c6093efa2fe69b328a866d
이진석 <tjrl1225@naver.com>
2024-07-25 15:44:30 +0900

    Feat: 유저 책목록 조회를 위한 세팅 Querydsl을 이용하여 쿼리하기 위해 custom repository를 세팅했습니다. 티켓: S11P12A603-131

---
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/fairyTale/repository/FairyTaleCustomRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.ssafy.a603.lingoland.fairyTale.repository;
+
+import static com.ssafy.a603.lingoland.fairyTale.entity.QFairyTale.fairyTale;
+import static com.ssafy.a603.lingoland.fairyTale.entity.QFairyTaleMember.fairyTaleMember;
+
+import java.util.List;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.a603.lingoland.fairyTale.dto.FairyTaleListResponseDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FairyTaleCustomRepositoryImpl implements FairyTaleCustomRepository {
+	private final JPAQueryFactory queryFactory;
+
+	public List<FairyTaleListResponseDTO> findAllFairyTalesByMemberId(int memberId) {
+		return queryFactory.select(
+				Projections.constructor(FairyTaleListResponseDTO.class,
+					fairyTale.id,
+					fairyTale.title,
+					fairyTale.cover,
+					fairyTale.summary
+				))
+			.from(fairyTale)
+			.join(fairyTaleMember).on(fairyTaleMember.fairyTale.id.eq(fairyTale.id))
+			.where(
+				isVisible(),
+				targetGroup(memberId)
+			).fetch();
+	}
+
+	private BooleanExpression isVisible() {
+		return fairyTaleMember.isVisible.eq(true);
+	}
+
+	private BooleanExpression targetGroup(int memberId) {
+		return fairyTaleMember.member.id.eq(memberId);
+	}
+}
