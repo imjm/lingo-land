618c825ee9266def5a8cf1e6490f6c5049697b45
김상혁 <ksang0728@gmail.com>
2024-07-26 11:52:01 +0900

    Feat: 로그인 관련 컴포넌트 위치 수정 - 로그인, 게임 슬라이드 컴포넌트 위치 수정 티켓: S11P12A603-334

---
+++ lingoland-frontend/src/components/authorization/login/Login.vue
@@ -0,0 +1,27 @@
+<script setup>
+import GenericInput from "../common/GenericInput.vue";
+import SubmitButton from "../common/SubmitButton.vue";
+</script>
+
+<template>
+    <GenericInput
+        v-model="userInfo.loginId"
+        type="text"
+        data="아이디"
+        id="loginId"
+        @blur-event="validateIDFormat(userInfo.loginId)"
+    />
+
+    <GenericInput
+        v-model="userInfo.password"
+        type="password"
+        data="비밀번호"
+        id="password"
+    />
+
+    <v-switch label="아이디저장" color="orange"></v-switch>
+
+    <SubmitButton id="login" data="로그인" width="100%" />
+</template>
+
+<style scoped></style>
