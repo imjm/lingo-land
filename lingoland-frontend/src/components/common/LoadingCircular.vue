b53f7b73bb820d59c0d5a24261aae3dc45ebc056
김상혁 <ksang0728@gmail.com>
2024-07-26 16:10:51 +0900

    Feat: 로딩페이지 컴포넌트 구현 - 로딩페이지 컴포넌트, 뷰를 구현했습니다. - 1차 티켓: S11P12A603-322

---
+++ lingoland-frontend/src/components/common/LoadingCircular.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+    data: String,
+});
+</script>
+
+<template>
+    <v-main class="d-flex align-center justify-center">
+        <div class="d-flex flex-column align-center justify-center mb-6">
+            <div class="text-h4 mb-9 font-weight-bold">
+                나만의 동화를 만들고 있어요...
+            </div>
+            <v-progress-circular
+                :size="400"
+                :width="40"
+                color="green"
+                indeterminate
+            ></v-progress-circular>
+        </div>
+    </v-main>
+</template>
+
+<style scoped></style>
---
+++ lingoland-frontend/src/views/LoadingPageView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoadingCircular from "@/components/common/LoadingCircular.vue";
+</script>
+
+<template>
+    <LoadingCircular data="나의 동화를 만들고 있어요" />
+</template>
+
+<style scoped></style>
