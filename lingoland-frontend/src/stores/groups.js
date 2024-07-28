c1de011207ef32ee0e08fec6d085bdb5e896801c
sooa <sooa971@gmail.com>
2024-07-28 10:38:25 +0900

    feat : FE - groupcreateAPI연결 group.js수정 groupcreateAPI연결설정 서버랑 통신은 안해봄 추가해야할 api있음 티켓: S11P12A603-345

--- lingoland-frontend/src/stores/groups.js
+++ lingoland-frontend/src/stores/groups.js
@@ -1,8 +1,85 @@
-import { ref, computed } from "vue";
+import { httpStatus } from "@/apis/http-status";
 import { defineStore } from "pinia";
-// import axios from 'axios'
+import swal from "sweetalert2";
+import { inject, ref } from "vue";
+import { useRouter } from "vue-router";
 
 export const useGroupStore = defineStore("group", () => {
+
+
+  /**
+   * State
+   */
+  window.Swal = swal;
+
+  const router = useRouter();
+  const axios = inject("axios");
+
+  /**
+   * actions
+  */
+
+  //그룹명 중복체크
+  const checkDuplicate = async (groupName) => {
+    let return_value;
+    await axios
+      .get(`/group/check/${groupName}`, { withCredentials: true })
+      .then((response) => {
+        if (response.status === httpStatus.OK) {
+          return_value = true;
+        }
+      })
+      .catch((error) => {
+        if (error.status === httpStatus.CONFLICT) {
+          return_value = false;
+        }
+      });
+
+    return return_value;
+  };
+
+
+
+
+  // 그룹 등록
+  const createGroup = async (groupInfo) => {
+    await axios
+      .post("/groups", groupInfo, { withCredentials: true })
+      .then((response) => {
+        if (response.status === httpStatus.CREATE) {
+          Swal.fire({
+            title: "그룹 만들기 성공!",
+            icon: "success",
+            confirmButtonText: "완료",
+          }).then(() => {
+            router.push({ name: "groupDetail", params : {groupId : 'back에서 줘야할듯'} });
+          });
+        } else {
+          Swal.fire({
+            title: "그룹 만들기 실패",
+            icon: "error",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // const groups = ref([]);
   const selectedGroup = ref(null);
 
@@ -35,3 +112,4 @@ export const useGroupStore = defineStore("group", () => {
 
   return { groups, clickGroup, selectedGroup };
 });
+
