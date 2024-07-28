9efc0089c3c0359c5eea543b7e516431334365da
tony <ksang0728@gmail.com>
2024-07-29 00:17:41 +0900

    Feat: 마이페이지 그룹 관련 버튼 라우팅 추가 - 그룹 가입, 만들기 버튼에 페이지로 이동하는 라우팅 추가 티켓: S11P12A603-342

--- lingoland-frontend/src/components/user/MyPage.vue
+++ lingoland-frontend/src/components/user/MyPage.vue
@@ -2,78 +2,86 @@
 import GenericButton from "@/components/common/GenericButton.vue";
 import PageNavigationButton from "@/components/common/PageNavigationButton.vue";
 import Profile from "@/components/common/Profile.vue";
-import GroupList from "../group/GroupList.vue";
-import SearchInput from "../common/SearchInput.vue";
-import IncorrectList from "@/components/incorrect/IncorrectList.vue";
 import incorrectDialog from "@/components/incorrect/IncorrectDialog.vue";
+import SearchInput from "../common/SearchInput.vue";
+import GroupList from "../group/GroupList.vue";
 
 import { ref } from "vue";
 
 const incorrectList = ref([
-  "문제 1",
-  "문제 2",
-  "문제 3",
-  "문제 4",
-  "문제 5",
-  "문제 6",
-  "문제 7",
-  "문제 8",
-  "문제 9",
-  "문제 10",
-  // 데이터 받아오기
+    "문제 1",
+    "문제 2",
+    "문제 3",
+    "문제 4",
+    "문제 5",
+    "문제 6",
+    "문제 7",
+    "문제 8",
+    "문제 9",
+    "문제 10",
+    // 데이터 받아오기
 ]);
+
+
+
 </script>
 
 <template>
-    <v-main class="d-flex justify-center">
-        <v-container>
-            <h1>MainPage View</h1>
-
+    <v-main class="d-flex mt-10 justify-center">
+        <v-container >
             <v-row>
                 <v-col cols="5">
                     <Profile source="src\\assets\\sampleImg.jpg" />
                 </v-col>
 
-        <v-col cols="7">
-          <v-row>
-            <v-sheet
-              rounded
-              class="ma-3"
-              :style="{ backgroundColor: '#CCCBFF' }"
-            >
-              <h1 class="mx-10">그룹 목록</h1>
-              <SearchInput />
-              <GroupList />
-            </v-sheet>
-          </v-row>
-          <v-row class="mr-1 d-flex justify-end">
-            <GenericButton
-              class="mr-3"
-              data="그룹 가입하기"
-              :style="{ backgroundColor: '#4C4637', color: 'white' }"
-            />
-            <GenericButton
-              data="그룹 만들기"
-              :style="{ backgroundColor: '#4C4637', color: 'white' }"
-            />
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-                <incorrectDialog :incorrects="incorrectList"/>
-     
-            </v-col>
-            <v-col cols="6">
-              <PageNavigationButton
-                background-color="#537960"
-                data="ㅇㅇㅇ의 동화"
-                source="src\assets\내동화.png"
-              />
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+                <v-col cols="7">
+                    <v-row>
+                        <v-sheet
+                            rounded
+                            class="ma-3"
+                            :style="{ backgroundColor: '#CCCBFF' }"
+                        >
+                            <h1 class="mx-10">그룹 목록</h1>
+                            <SearchInput />
+                            <GroupList />
+                        </v-sheet>
+                    </v-row>
+                    <v-row class="mr-1 d-flex justify-end">
+                        <GenericButton
+                            class="mr-3"
+                            data="그룹 가입하기"
+                            :style="{
+                                backgroundColor: '#4C4637',
+                                color: 'white',
+                            }"
+                            @click-event="$router.push({name: 'groupList'})"
+                        />
+                        <GenericButton
+                            data="그룹 만들기"
+                            :style="{
+                                backgroundColor: '#4C4637',
+                                color: 'white',
+                            }"
+                            @click-event="$router.push({name: 'groupCreate'})"
+                        />
+                    </v-row>
+                    <v-row>
+                        <v-col cols="6">
+                            <incorrectDialog :incorrects="incorrectList" />
+                        </v-col>
+
+                        <v-col cols="6">
+                            <PageNavigationButton
+                                background-color="#537960"
+                                data="ㅇㅇㅇ의 동화"
+                                source="src\assets\내동화.png"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 </template>
 
 <style scoped>
