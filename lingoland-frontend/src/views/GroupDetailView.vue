f503a33ee5219aa1881d930a1913f3efce786af8
배수아 <sooa971@gmail.com>
2024-08-01 13:29:26 +0900

    feat : FE-전체 layout수정 티켓: S11P12A603-429

--- lingoland-frontend/src/views/GroupDetailView.vue
+++ lingoland-frontend/src/views/GroupDetailView.vue
@@ -1,21 +1,18 @@
 <script setup>
 import GroupMemberList from "@/components/group/GroupMemberList.vue";
-
 </script>
 
 <template>
     <main class="mt-10">
         <v-row>
-            <v-col cols="2">
-
-            </v-col>
+            <v-col cols="2"> </v-col>
             <v-col cols="8">
                 <v-sheet class="g-10">
                     <v-row>
                         <v-col cols="5">
                             <h1 class="mx-5 my-0">group.name</h1>
                         </v-col>
-                        <v-col >
+                        <v-col>
                             <v-autocomplete
                                 :items="items"
                                 append-inner-icon="mdi-microphone"
