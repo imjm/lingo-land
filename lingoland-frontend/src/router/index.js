68c122a3a85652ee329ba18e2b7b79e747ffbb06
김상혁 <ksang0728@gmail.com>
2024-08-05 16:00:03 +0900

    Feat: View 디렉토리 리팩토링 - View를 디렉토리별로 리팩토링했습니다. - 리팩토링 기준으로 index.js를 수정했습니다. 티켓: S11P12A603-472

--- lingoland-frontend/src/router/index.js
+++ lingoland-frontend/src/router/index.js
@@ -2,8 +2,8 @@ import GroupMemberDetail from "@/components/group/GroupMemberDetail.vue";
 import GroupMemberDetailByAdmin from "@/components/group/GroupMemberDetailByAdmin.vue";
 import MyPage from "@/components/user/MyPage.vue";
 import GameRoomView from "@/views/GameRoomView.vue";
-import GroupListView from "@/views/GroupListView.vue";
-import GroupView from "@/views/GroupView.vue";
+import GroupListView from "@/views/group/GroupListView.vue";
+import GroupView from "@/views/group/GroupView.vue";
 import LoginView from "@/views/LoginView.vue";
 import MainPageView from "@/views/MainPageView.vue";
 import MyPageView from "@/views/MyPageView.vue";
@@ -11,129 +11,139 @@ import SignUpView from "@/views/SignUpView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
 
-  routes: [
-    {
-      path: "/",
-      redirect: "/login", // 기본 경로를 로그인 페이지로 리디렉션
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/signup",
-      name: "signUp",
-      component: SignUpView,
-    },
-    {
-      path: "/game-room",
-      component: GameRoomView,
-      children: [
+    routes: [
         {
-          path: ":roomId",
-          name: "gameRoom",
-          component: () => import("@/components/gameRoom/TheGameRoom.vue"),
+            path: "/",
+            redirect: "/login", // 기본 경로를 로그인 페이지로 리디렉션
         },
         {
-          path: ":roomId/writing-game",
-          name: "writingGame",
-          component: () => import("@/views/WritingGameView.vue"),
+            path: "/login",
+            name: "login",
+            component: LoginView,
         },
         {
-          path: ":roomId/writing-game/result",
-          name: "writingGameResult",
-          component: () => import("@/views/WritingGameResultView.vue"),
+            path: "/signup",
+            name: "signUp",
+            component: SignUpView,
         },
+        {
+            path: "/game-room",
+            component: GameRoomView,
+            children: [
+                {
+                    path: ":roomId",
+                    name: "gameRoom",
+                    component: () =>
+                        import("@/components/gameRoom/TheGameRoom.vue"),
+                },
+                {
+                    path: ":roomId/writing-game",
+                    name: "writingGame",
+                    component: () =>
+                        import("@/views/writingGame/WritingGameView.vue"),
+                },
+                {
+                    path: ":roomId/writing-game/result",
+                    name: "writingGameResult",
+                    component: () =>
+                        import("@/views/writingGame/WritingGameResultView.vue"),
+                },
 
-        {
-          path: ":roomId/running-game",
-          name: "runningGame",
-          component: () => import("@/views/RunningGameView.vue"),
+                {
+                    path: ":roomId/running-game",
+                    name: "runningGame",
+                    component: () =>
+                        import("@/views/runningGame/RunningGameView.vue"),
+                },
+                {
+                    path: ":roomId/running-game/result",
+                    name: "runningGameResult",
+                    component: () =>
+                        import("@/views/runningGame/RunningGameResultView.vue"),
+                },
+            ],
         },
         {
-          path: ":roomId/running-game/result",
-          name: "runningGameResult",
-          component: () => import("@/views/RunningGameResultView.vue"),
-        },
-      ],
-    },
-    {
-      path: "/main-page",
-      name: "mainPage",
-      component: MainPageView,
-    },
-    {
-      path: "/my-page",
-      component: MyPageView,
-      children: [
-        {
-          path: "",
-          name: "myPage",
-          component: MyPage,
+            path: "/main-page",
+            name: "mainPage",
+            component: MainPageView,
         },
         {
-          path: "modify",
-          name: "myPageModify",
-          component: () => import("@/components/user/MyPageModify.vue"),
+            path: "/my-page",
+            component: MyPageView,
+            children: [
+                {
+                    path: "",
+                    name: "myPage",
+                    component: MyPage,
+                },
+                {
+                    path: "modify",
+                    name: "myPageModify",
+                    component: () =>
+                        import("@/components/user/MyPageModify.vue"),
+                },
+                {
+                    path: "modify/password",
+                    name: "myPageModifyPassword",
+                    component: () =>
+                        import("@/components/user/PasswordModify.vue"),
+                },
+                {
+                    path: "book",
+                    name: "bookList",
+                    component: () => import("@/views/tale/TaleListView.vue"),
+                },
+                {
+                    path: "book/:bookId",
+                    name: "bookDetail",
+                    component: () => import("@/views/tale/TalesDetailView.vue"),
+                },
+            ],
         },
         {
-          path: "modify/password",
-          name: "myPageModifyPassword",
-          component: () => import("@/components/user/PasswordModify.vue"),
+            path: "/group",
+            component: GroupView,
+            children: [
+                {
+                    path: "list",
+                    name: "groupList",
+                    component: GroupListView,
+                },
+                {
+                    path: "list/:groupId",
+                    name: "groupDetail",
+                    component: () =>
+                        import("@/views/group/GroupDetailView.vue"),
+                },
+                {
+                    path: "create",
+                    name: "groupCreate",
+                    component: () =>
+                        import("@/components/group/GroupCreate.vue"),
+                },
+                {
+                    path: "modify",
+                    name: "groupModify",
+                    component: () =>
+                        import("@/components/group/GroupModify.vue"),
+                },
+                {
+                    path: "member/:memberId",
+                    name: "groupMemberDetail",
+                    component: GroupMemberDetail,
+                },
+                {
+                    path: "member/:memberId",
+                    name: "groupMemberDetailByAdmin",
+                    component: GroupMemberDetailByAdmin,
+                    //그룹장만 입장 가능하게 해야 되맨!
+                },
+            ],
         },
-        {
-          path: "book",
-          name: "bookList",
-          component: () => import("@/views/TaleListView.vue"),
-        },
-        {
-          path: "book/:bookId",
-          name: "bookDetail",
-          component: () => import("@/views/TalesDetailView.vue"),
-        },
-      ],
-    },
-    {
-      path: "/group",
-      component: GroupView,
-      children: [
-        {
-          path: "list",
-          name: "groupList",
-          component: GroupListView,
-        },
-        {
-          path: "list/:groupId",
-          name: "groupDetail",
-          component: () => import("@/views/GroupDetailView.vue"),
-        },
-        {
-          path: "create",
-          name: "groupCreate",
-          component: () => import("@/components/group/GroupCreate.vue"),
-        },
-        {
-          path: "modify",
-          name: "groupModify",
-          component: () => import("@/components/group/GroupModify.vue"),
-        },
-        {
-          path: "member/:memberId",
-          name: "groupMemberDetail",
-          component: GroupMemberDetail,
-        },
-        {
-          path: "member/:memberId",
-          name: "groupMemberDetailByAdmin",
-          component: GroupMemberDetailByAdmin,
-          //그룹장만 입장 가능하게 해야 되맨!
-        },
-      ],
-    },
-  ],
+    ],
 });
 
 export default router;
