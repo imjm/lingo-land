650ebe3f290a0af605f5c88903b6e2139b8c0e1a
김상혁 <ksang0728@gmail.com>
2024-08-05 18:14:18 +0900

    Feat: 글쓰기 게임 dialog 개발 중 티켓: S11P12A603-472

---
+++ lingoland-frontend/src/components/writingGame/WritingGameSetDialog.vue
@@ -0,0 +1,34 @@
+<script setup>
+import GenericButton from "../common/GenericButton.vue";
+import GenericInput from "../common/GenericInput.vue";
+import GenericInputArea from "../common/GenericInputArea.vue";
+import SubmitButton from "../common/SubmitButton.vue";
+
+const model = defineModel();
+
+function setPageCount() {
+    model.value.isOpen = false;
+}
+</script>
+
+<template>
+    <v-dialog v-model="model.isOpen" width="850" height="600">
+        <v-sheet width="500" height="500" class="d-flex justify-center align-center pa-15">
+            <v-row>
+                <v-col>
+                    <div>몇 페이지 작성할 것인지 써주세요</div>
+                    <h3>비밀번호</h3>
+                    <GenericInput type="password" />
+                    <SubmitButton
+                        class="ms-auto d-flex align-left"
+                        data="가입하기"
+                        width="100%"
+                        @click-event="setPageCount()"
+                    />
+                </v-col>
+            </v-row>
+        </v-sheet>
+    </v-dialog>
+</template>
+
+<style scoped></style>
--- lingoland-frontend/src/views/writingGame/WritingGameView.vue
+++ lingoland-frontend/src/views/writingGame/WritingGameView.vue
@@ -1,27 +1,34 @@
 <script setup>
+import WritingGameSetDialog from "@/components/writingGame/WritingGameSetDialog.vue";
 import WritingInput from "@/components/writingGame/WritingInput.vue";
 import WritingList from "@/components/writingGame/WritingList.vue";
+import { ref } from "vue";
+
+const dialog = ref({
+    totalpage: Number,
+    isOpen: true,
+});
 </script>
 
 <template>
-    <v-layout>
-        <v-main
-            class="background-image d-flex flex-column align-center justify-center"
-        >
-            <div class="text-h3 text-white my-16 pt-5">
-                다음 글을 읽고 이어질 이야기를 작성하세요
-            </div>
+    <v-main
+        class="background-image d-flex flex-column align-center justify-center"
+    >
+        <div class="text-h3 text-white my-16 pt-5">
+            다음 글을 읽고 이어질 이야기를 작성하세요
+        </div>
 
-            <v-row class="d-flex justify-space-evenly">
-                <v-col class="d-flex ma-10 pa-10 align-start">
-                    <WritingList />
-                </v-col>
-                <v-col class="d-flex align-start ma-10 pa-5">
-                    <WritingInput />
-                </v-col>
-            </v-row>
-        </v-main>
-    </v-layout>
+        <v-row class="d-flex justify-space-evenly">
+            <v-col class="d-flex ma-10 pa-10 align-start">
+                <WritingList />
+            </v-col>
+            <v-col class="d-flex align-start ma-10 pa-5">
+                <WritingInput />
+            </v-col>
+        </v-row>
+
+        <WritingGameSetDialog v-model="dialog" />
+    </v-main>
 </template>
 
 <style scoped>
