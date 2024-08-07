f9312002e0cdc4f550b670a4873b92e09f82da7d
김상혁 <ksang0728@gmail.com>
2024-08-07 11:48:51 +0900

    Feat: 학습하기 버튼 클릭 시 - 학습하기 버튼 클릭 시 보기에 답과 내가 선택한 답 색상 변경 및 해설 보이기 - 학습완료 버튼 이벤트 리스너 등록 티켓: S11P12A603-492

--- lingoland-frontend/src/components/incorrect/IncorrectListItem.vue
+++ lingoland-frontend/src/components/incorrect/IncorrectListItem.vue
@@ -1,47 +1,100 @@
 <script setup>
-import { defineProps } from "vue";
+import { useProblemStore } from "@/stores/problem";
+import { defineProps, ref } from "vue";
 import GenericButton from "../common/GenericButton.vue";
+
+const emit = defineEmits(["clickEvent"]);
 
 const props = defineProps({
     incorrect: Object,
 });
+
+const problemStore = useProblemStore();
+
+const isStudy = ref(false);
+const answerColor = ref("");
+const myAnswerColor = ref("");
+
+function studyProblem() {
+    // 학습하기 버튼을 누르면 답변에 색칠
+    // 정답은 초록색, 내가 선택한 답은 빨간색
+    // 문제 해설 보이기
+    isStudy.value = !isStudy.value;
+
+    if (isStudy.value) {
+        answerColor.value = "#00C853";
+        myAnswerColor.value = "#FF1744";
+    }
+}
+
+function completeProblem(problemId) {
+    const result = problemStore.delectMyWrongProblem(problemId);
+
+    if (result) {
+        emit("clickEvent");
+    }
+}
 </script>
 
 <template>
-    <v-list-item-content>
-        <v-container class="border-md">
-            <v-row class="d-flex flex-column">
+    <v-container class="border-md">
+        <v-row class="d-flex flex-column">
+            <v-col>
+                <div class="text-h4">{{ incorrect.problem }}</div>
+            </v-col>
+
+            <template v-for="(choice, index) in incorrect.choices" :key="index">
                 <v-col>
-                    <div class="text-h4">{{ incorrect.problem }}</div>
+                    <div
+                        class="text-h5"
+                        :style="{
+                            color:
+                                isStudy && choice.num === incorrect.answer
+                                    ? answerColor
+                                    : isStudy &&
+                                      choice.num === incorrect.submittedAnswer
+                                    ? myAnswerColor
+                                    : '',
+                        }"
+                    >
+                        {{ choice.num }}. {{ choice.text }}
+                    </div>
                 </v-col>
+            </template>
+
+            <template v-if="isStudy">
                 <v-col>
+                    <div class="text-h6">문제 해설</div>
                     <div class="text-h6">
-                        문제 해설: {{ incorrect.explanation }}
+                        {{ incorrect.explanation }}
                     </div>
                 </v-col>
-                <v-col>
-                    <div class="text-h5">1</div>
-                </v-col>
-                <v-col>
-                    <div class="text-h5">2</div>
-                </v-col>
-                <v-col>
-                    <div class="text-h5">3</div>
-                </v-col>
-                <v-col>
-                    <div class="d-flex justify-end">
-                        <GenericButton
-                            :style="{
-                                backgroundColor: '#4C4637',
-                                color: 'white',
-                            }"
-                            data="학습완료"
-                        />
-                    </div>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-list-item-content>
+            </template>
+
+            <v-col>
+                <div class="d-flex justify-end">
+                    <GenericButton
+                        :style="{
+                            backgroundColor: '#4C4637',
+                            color: 'white',
+                        }"
+                        class="mr-5"
+                        data="학습하기"
+                        @click-event="studyProblem"
+                    />
+
+                    <GenericButton
+                        :style="{
+                            backgroundColor: '#ECEFF1',
+                            color: 'black',
+                        }"
+                        data="학습완료"
+                        @click-event="completeProblem(incorrect.problemId)"
+                    />
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <style scoped>
