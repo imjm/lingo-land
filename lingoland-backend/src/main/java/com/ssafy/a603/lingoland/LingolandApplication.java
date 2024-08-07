10cb1c9019d646c867b82398131f3903ab501fd5
임준혁 <junhyuk1027@naver.com>
2024-08-07 15:27:16 +0900

    Revert "Merge branch 'feature/470-설정-변경' into 'develop'"
    This reverts merge request !212

--- lingoland-backend/src/main/java/com/ssafy/a603/lingoland/LingolandApplication.java
+++ lingoland-backend/src/main/java/com/ssafy/a603/lingoland/LingolandApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class LingolandApplication {
 
 	public static void main(String[] args) {
-		System.out.println("********************************");
 		SpringApplication.run(LingolandApplication.class, args);
 	}
 
