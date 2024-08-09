be3ac1ee198cb063a8e1034f60ea53ab6b5eadd4
김상혁 <ksang0728@gmail.com>
2024-08-09 17:58:59 +0900

    Feat: 더미데이터 삭제 티켓: S11P12A603-474

--- lingoland-frontend/src/components/writingGame/TheWritingGame.vue
+++ lingoland-frontend/src/components/writingGame/TheWritingGame.vue
@@ -3,87 +3,11 @@ import WritingInput from "@/components/writingGame/WritingInput.vue";
 import WritingList from "@/components/writingGame/WritingList.vue";
 import { useOpenviduStore } from "@/stores/openvidu";
 import { storeToRefs } from "pinia";
-import { onMounted, ref } from "vue";
 
 const openviduStore = useOpenviduStore();
 
 const { OV, session } = openviduStore;
 const { participants } = storeToRefs(openviduStore);
-
-// 제출을 눌렀을 때 시그널을 송신
-
-// 제출을 눌렀을 때 시그널을 수신
-
-onMounted(() => {
-    // 참가자 배열 만큼 이야기 배열을 만듬
-
-    // 이야기 마다 작성할 유저의 순서를 미리 만들기
-    // 방장의 순서를 기준으로 만든다 -> 유저마다 참가자 배열의 순서가 다르기 때문
-    const storyOrdering = ref([
-        {
-            storyId: 1,
-            order: [1, 2, 3, 4, 5],
-            visited: [false, false, false, false, false],
-        },
-        {
-            storyId: 2,
-            order: [2, 3, 4, 5, 1],
-            visited: [false, false, false, false, false],
-        },
-        {
-            storyId: 3,
-            order: [3, 4, 5, 1, 2],
-            visited: [false, false, false, false, false],
-        },
-        {
-            storyId: 4,
-            order: [4, 5, 1, 2, 3],
-            visited: [false, false, false, false, false],
-        },
-        {
-            storyId: 5,
-            order: [5, 1, 2, 3, 4],
-            visited: [false, false, false, false, false],
-        },
-    ]);
-
-    // 이야기 배열 구성
-    const storyList = ref([
-        {
-            storyId: 1,
-            story: [
-                {
-                    content: "첫번째 스토리",
-                },
-                {
-                    content: "두번째 스토리",
-                },
-            ],
-        },
-        {
-            storyId: 2,
-            story: [
-                {
-                    content: "첫번째 스토리",
-                },
-                {
-                    content: "두번째 스토리",
-                },
-            ],
-        },
-        {
-            storyId: 3,
-            story: [
-                {
-                    content: "첫번째 스토리",
-                },
-                {
-                    content: "두번째 스토리",
-                },
-            ],
-        },
-    ]);
-});
 </script>
 
 <template>
