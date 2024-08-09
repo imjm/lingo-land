df29f82d2be26e973720c562f9c29522552733d8
김상혁 <ksang0728@gmail.com>
2024-08-09 17:59:43 +0900

    Feat: 이야기 배열, 이야기 순서 배열 초기화 함수 추가 티켓: S11P12A603-474

--- lingoland-frontend/src/stores/openvidu.js
+++ lingoland-frontend/src/stores/openvidu.js
@@ -13,7 +13,10 @@ export const useOpenviduStore = defineStore("openvidu", () => {
     const userStore = useUserStore();
     const participants = ref([]);
     const mynum = ref("");
-    const reparticipants = ref([])
+
+    const reparticipants = ref([]);
+    const storyList = ref([]);
+    const storyOrderList = ref([]);
 
     // 세션에 스트림이 생성될 때 호출되는 콜백 함수
     session.on("streamCreated", function (event) {
@@ -53,15 +56,16 @@ export const useOpenviduStore = defineStore("openvidu", () => {
     session.on("signal:gameStart", function (event) {
         const gameType = JSON.parse(event.data);
 
+        // 참가자 커넥션 아이디를 기준으로 정렬
+        reparticipants.value = participants.value.sort((a, b) => {
+            return a.connectionId.localeCompare(b.connectionId);
+        });
+        console.log("오픈비두 참가자목록 보기", participants);
+        console.log("재정렬한참가자들!!!!!!!!!!!!!!!!!", reparticipants.value);
+
         // 달리기 게임으로
         if (gameType.type === 1) {
-          console.log('오픈비두 참가자목록 보기',participants)
-          reparticipants.value = participants.value.sort((a,b) => {
-            return a.connectionId.localeCompare(b.connectionId)
-          })
-          console.log('재정렬한참가자들!!!!!!!!!!!!!!!!!',reparticipants.value)
-
-          for (let i = 0; i < reparticipants.value.length; i++) {
+            for (let i = 0; i < reparticipants.value.length; i++) {
                 if (
                     reparticipants.value[i].connectionId ==
                     event.target.connection.connectionId
@@ -72,6 +76,12 @@ export const useOpenviduStore = defineStore("openvidu", () => {
             }
             router.replace({ name: "runningGame" });
         } else if (gameType.type === 2) {
+            // 이야기 배열 초기화
+            initalizeStoryList();
+
+            // 정렬된 참가자 아이디를 기준으로 이야기 작성 순서 초기화
+            initalizeOrderList();
+
             // 글쓰기 게임으로
             router.replace({ name: "writingGame" });
         }
@@ -101,6 +111,7 @@ export const useOpenviduStore = defineStore("openvidu", () => {
         participants.value = [];
     }
 
+    // 방장 확인
     function isLeader() {
         console.log("************isLeader", participants.value);
 
@@ -111,13 +122,42 @@ export const useOpenviduStore = defineStore("openvidu", () => {
         return isLeader;
     }
 
+    // 이야기 배열 초기화
+    function initalizeStoryList() {
+        for (let index = 0; index < reparticipants.value.length; index++) {
+            storyList.value.push({ storyId: index, story: [] });
+        }
+    }
+
+    // 이야기 작성 배열 초기화
+    function initalizeOrderList() {
+        for (let index = 0; index < reparticipants.value.length; index++) {
+            // 선형적으로 이야기 순서 만들기
+            let orderList = [];
+            for (
+                let orderIndex = index;
+                orderIndex < orderIndex + reparticipants.value.length;
+                orderIndex++
+            ) {
+                orderList.push(orderIndex % reparticipants.value.length);
+            }
+
+            storyOrderList.value.push({
+                connectionId: reparticipants.value[index].connectionId,
+                storyOrderList: orderList,
+            });
+        }
+    }
+
     return {
         OV,
         session,
         participants,
+        reparticipants,
         mynum,
+        storyList,
+        storyOrderList,
         resetParticipants,
         isLeader,
-        reparticipants
     };
 });
