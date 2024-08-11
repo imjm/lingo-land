8fa76a269871d0e88cbe8ff705d139c46eb5d571
tony <ksang0728@gmail.com>
2024-08-12 00:16:37 +0900

    Feat: 글쓰기 제출 기능 추가 - 글쓰기 제출 버튼을 눌렀을 때 API 요청 - 타이머 종료시 API 요청, 시그널 송신(글쓰기 게임 중, 글쓰기 게임 종료) 티켓: S11P12A603-454

--- lingoland-frontend/src/components/writingGame/WritingInput.vue
+++ lingoland-frontend/src/components/writingGame/WritingInput.vue
@@ -1,14 +1,87 @@
 <script setup>
+import { useOpenviduStore } from "@/stores/openvidu";
+import { useWritingGameStore } from "@/stores/writingGame";
+import { storeToRefs } from "pinia";
 import { ref } from "vue";
-import CountdownTimer from "./CountdownTimer.vue";
-import SubmitButton from "../common/SubmitButton.vue";
+import { useRoute } from "vue-router";
 import GenericInputArea from "../common/GenericInputArea.vue";
+import SubmitButton from "../common/SubmitButton.vue";
+import CountdownTimer from "./CountdownTimer.vue";
+
+const route = useRoute();
+
+const writingGameStore = useWritingGameStore();
+const { turn, pageCount } = storeToRefs(writingGameStore);
+
+const openviduStore = useOpenviduStore();
+const { session, findCurrentStoryId, findMyLoginId } = openviduStore;
 
 const textareaValue = ref("");
+const isSubmit = ref(false);
 
 const handleTimesUp = () => {
-    console.log("타이머가 종료되었습니다. 텍스트 내용:", textareaValue.value);
+    // 타임아웃이므로 강제로 제출
+    // 아직 제출하지 않았으면 제출시킨다.
+    if (!isSubmit.value) {
+        sumbit();
+    }
+
+    isSubmit.value = false;
+
+    let currentStoryId = findCurrentStoryId();
+
+    console.log("*************current storyId: ", currentStoryId);
+
+    // 시그널 보내기
+    if (turn.value + 1 < pageCount.value) {
+        session
+            .signal({
+                type: "writingGame",
+                data: JSON.stringify({
+                    storyId: currentStoryId,
+                    story: textareaValue.value,
+                }),
+            })
+            .then(() => {
+                console.log("******************writingGame submit");
+            })
+            .catch((error) => {
+                console.error("***************error sending signal", error);
+            });
+    } else {
+        // 마지막 페이지일 때 글쓰기 게임 종료
+        session
+            .signal({ type: "gameEnd", data: JSON.stringify({ type: 2 }) })
+            .then(() => {
+                console.log("***********************writingGame end");
+            })
+            .catch((error) => {
+                console.error("******************error sending signal", error);
+            });
+    }
+
+    // textarea 초기화하기
+    textareaValue.value = "";
+
+    // turn 증가하기
+    turn.value++;
 };
+
+function sumbit() {
+    let myLoginId = findMyLoginId();
+
+    const drawingDTO = {
+        key: myLoginId, // 로그인 아이디
+        story: textareaValue.value,
+        order: turn.value,
+    };
+
+    console.log("*******drawDTO", drawingDTO);
+    // API 요청 보내기
+    // writingGameStore.submitStory(route.params.roomId, drawingDTO)
+
+    isSubmit.value = true;
+}
 </script>
 
 <template>
@@ -18,7 +91,7 @@ const handleTimesUp = () => {
         <GenericInputArea v-model="textareaValue" />
 
         <div class="d-flex justify-end">
-            <SubmitButton width="100" data="제출" />
+            <SubmitButton width="100" data="제출" @click-event="sumbit" />
         </div>
     </v-card>
 </template>
