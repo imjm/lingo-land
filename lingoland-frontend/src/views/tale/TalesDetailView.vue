d4b35adad1641056c565bacf4d5a6dec5e10b502
배수아 <sooa971@gmail.com>
2024-08-12 17:57:54 +0900

    feat : 글쓰기 결과 수정중

--- lingoland-frontend/src/views/tale/TalesDetailView.vue
+++ lingoland-frontend/src/views/tale/TalesDetailView.vue
@@ -1,14 +1,37 @@
 <script setup>
+import { ref } from "vue";
 import BackButton from "@/components/common/BackButton.vue";
 import TaleDetail from "@/components/tale/TaleDetail.vue";
+import bookStore from "/bookStore.jpg";
+
+// 밝기 상태를 관리하는 ref 생성
+const isDimmed = ref(false);
+
+// 배경을 어둡게/밝게 전환하는 함수
+function toggleDim() {
+    isDimmed.value = !isDimmed.value;
+}
 </script>
 
 <template>
-    <BackButton />
-    
-    <TaleDetail class="gowun-batang-regular"/>
+    <div :class="['main', { dimmed: isDimmed }]" @click="toggleDim">
+        <BackButton />
+
+        <TaleDetail class="gowun-batang-regular" />
+    </div>
 </template>
 
 <style scoped>
+.main {
+    background-image: url(/bookStore.jpg);
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+    transition: filter 1s ease; /* 필터 애니메이션 적용 */
+}
 
+/* 어두운 상태에 적용될 클래스 */
+.dimmed {
+    filter: brightness(50%);
+}
 </style>
