20f4679c278a764bdc1be0774ceda4ad6053f36f
배수아 <sooa971@gmail.com>
2024-08-12 17:34:49 +0900

    feat : 글 리스트 수정 됨.......휴

--- lingoland-frontend/src/views/tale/TaleListView.vue
+++ lingoland-frontend/src/views/tale/TaleListView.vue
@@ -1,18 +1,22 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import BackButton from "@/components/common/BackButton.vue";
 import TaleList from "@/components/tale/TaleList.vue";
+
+
 </script>
 
 <template>
-    <div style="background-color: white;">
-
+    <div class="main">
         <BackButton />
-        <div class="d-flex justify-center align-center" >
-            <TaleList class="my-3" />
-        </div>
+        <TaleList class="my-3" />
     </div>
 </template>
 
 <style scoped>
-
+.main {
+    height: 100vh;
+    width: 100vw;
+    background-color: white;
+}
 </style>
