1b6580ab9a44ba93b02ea327e5231d5551420aa3
김상혁 <ksang0728@gmail.com>
2024-08-13 11:21:41 +0900

    Feat: 글쓰기 게임 UI 수정 티켓: S11P12A603-529

--- lingoland-frontend/src/components/writingGame/WritingInput.vue
+++ lingoland-frontend/src/components/writingGame/WritingInput.vue
@@ -5,7 +5,6 @@ import { storeToRefs } from "pinia";
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 import GenericInputArea from "../common/GenericInputArea.vue";
-import SubmitButton from "../common/SubmitButton.vue";
 import CountdownTimer from "./CountdownTimer.vue";
 
 const route = useRoute();
@@ -86,23 +85,29 @@ function sumbit() {
 
 <template>
     <div class="d-flex flex-column justify-center align-center">
-        <div class="d-flex justify-end mb-10" style="font-size: 30px; color: white">
+        <div
+            class="d-flex justify-end mb-10"
+            style="font-size: 30px; color: white"
+        >
             다음 글을 읽고 이어질 이야기를 작성하세요
         </div>
-        <v-card class="d-flex flex-column justify-center align-center" width="500" height="500"
-            style="background-color: #ffe280">
+        <v-card
+            class="d-flex flex-column justify-center align-center"
+            width="500"
+            height="500"
+            style="background-color: #ffe280"
+        >
             <CountdownTimer @timesUp="handleTimesUp" />
 
-            <v-textarea
+            <GenericInputArea
                 class="textarea custom-textarea"
                 v-model="textareaValue"
                 placeholder="글을 작성해주세요"
+            />
+            <!-- <v-textarea
+                placeholder="글을 작성해주세요"
                 hide-details
-            ></v-textarea>
-
-            <div class="d-flex justify-end">
-                <SubmitButton width="100" data="제출" class="mx-5 mb-5" />
-            </div>
+            ></v-textarea> -->
         </v-card>
     </div>
 </template>
--- lingoland-frontend/src/components/writingGame/WritingList.vue
+++ lingoland-frontend/src/components/writingGame/WritingList.vue
@@ -63,23 +63,49 @@ const computedStorys = computed(() => {
 </script>
 
 <template>
-
-    <div
-        v-for="(story, index) in computedStorys"
-        :key="index"
-        class="image d-flex justify-center align-center"
-    >
-        {{ story }}
+    <div class="scroll">
+        <div
+            class="story"
+            v-for="(story, index) in computedStorys"
+            :key="index"
+        >
+            <div style="font-weight: 700; width: 80%">
+                {{ story }}
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.image {
-    background-image: url("/balloon1.png");
-    /* height: 200px; */
-    /* width: 80%; */
-    background-size: cover;
-    background-position: center;
+.scroll {
+    position: absolute;
+    top: 10%;
+    max-height: 70%;
+    overflow-y: auto;
+    overflow-x: hidden;
+    width: 80%;
+}
 
+.story {
+    position: relative;
+    top: 10%;
+    left: 20%;
+    margin-bottom: 20px;
+}
+
+.scroll::-webkit-scrollbar {
+    width: 5px;
+    /* 스크롤바의 너비 */
+}
+
+.scroll::-webkit-scrollbar-track {
+    background: rgb(255, 255, 255, 1);
+}
+
+.scroll::-webkit-scrollbar-thumb {
+    background: #c2c2c2;
+    /* 핸들의 색상 */
+    border-radius: 10px;
+    /* 핸들의 모서리 둥글기 */
 }
 </style>
