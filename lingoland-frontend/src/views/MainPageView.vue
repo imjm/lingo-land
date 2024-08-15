dc5a6efaf67a9cb5b42bb2a301c04028d9a214a3
tony <ksang0728@gmail.com>
2024-08-16 01:57:51 +0900

    Feat: main page css 수정

--- lingoland-frontend/src/views/MainPageView.vue
+++ lingoland-frontend/src/views/MainPageView.vue
@@ -1,17 +1,15 @@
 <script setup>
+import makingRoomImg from "@/assets/animal/ANIMALS2.png";
 import GenericButton from "@/components/common/GenericButton.vue";
 import GenericInput from "@/components/common/GenericInput.vue";
 import MyPageButton from "@/components/common/MyPageButton.vue";
 import PageNavigationButton from "@/components/common/PageNavigationButton.vue";
 import Profile from "@/components/common/Profile.vue";
-import RankList from "@/components/rank/RankList.vue";
 import { useGameRoomStore } from "@/stores/gameRoom";
 import { useOpenviduStore } from "@/stores/openvidu";
 import { useGameStore } from "@/stores/runningGame/gameStore";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import makingRoomImg from "@/assets/animal/ANIMALS2.png"
-
 
 const router = useRouter();
 
@@ -88,49 +86,34 @@ function joinRoom() {
                     />
                 </v-col>
 
-                <v-col cols="7">
-                    <v-row
-                    >
-                        <v-col
-                            cols="6"
-                            class="d-flex flex-column justify-space-between"
-                        >
-                            <div>
-                                <PageNavigationButton
-                                    background-color="#CCCBFF"
-                                    data="방 만들기"
-                                    :source="makingRoomImg"
-                                    @click-event="makeRoom"
-                                    height="45vh"
-                                />
-                            </div>
-
-                            
+                <v-col cols="7" class="d-flex justify-center">
+                    <v-row>
+                        <v-col>
+                            <PageNavigationButton
+                                background-color="#CCCBFF"
+                                data="방 만들기"
+                                :source="makingRoomImg"
+                                @click-event="makeRoom"
+                                height="100%"
+                            />
                         </v-col>
 
-                        <v-col cols="6">
-                            <div
-                                class="room-code d-flex flex-column justify-center align-center"
-                            >
-                                <div
-                                    class="ma-3"
-                                    style="
-                                        font-size: xx-large;
-                                        font-weight: 700;
-                                    "
-                                >
-                                    방 코드
-                                </div>
-                                <GenericInput
-                                    v-model="roomCode"
-                                    :style="{ width: '90%' }"
-                                />
-                                <GenericButton
-                                    @click-event="joinRoom"
-                                    data="입력"
-                                />
+                        <v-col
+                            class="room-code d-flex flex-column justify-center align-center ma-3"
+                        >
+                            <div style="font-size: xx-large; font-weight: 700">
+                                방 코드
                             </div>
-                        
+                            <GenericInput
+                                v-model="roomCode"
+                                :style="{ width: '90%' }"
+                                @keyUp-event="joinRoom"
+                            />
+                            <GenericButton
+                                data="입력"
+                                :style="{ width: '90%' }"
+                                @click-event="joinRoom"
+                            />
                         </v-col>
                     </v-row>
                 </v-col>
@@ -142,8 +125,6 @@ function joinRoom() {
 <style scoped>
 .room-code {
     background-color: #d2f0ff;
-    width: 100%;
-    height: 300px;
     border-radius: 4px;
     font-size: x-large;
 }
