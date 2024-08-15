f188f66aa1c3448a9d2eb5bdeb2f5bdcaee9cc96
tony <ksang0728@gmail.com>
2024-08-16 03:02:46 +0900

    Feat: favicon 설정 및 웹페이지 이름 설정

--- lingoland-frontend/src/App.vue
+++ lingoland-frontend/src/App.vue
@@ -3,37 +3,6 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <!-- <RouterLink :to="{ name: 'login' }"> login</RouterLink> |
-  <RouterLink :to="{ name: 'gameRoom', params: { roomId: '1' } }">
-    gameRoom</RouterLink
-  >
-  | <RouterLink :to="{ name: 'signUp' }"> signup</RouterLink> |  -->
-<!-- 
-  <RouterLink :to="{ name: 'writingGame', params: { roomId: '1' } }">
-    writingGame</RouterLink
-  >
-  |
-  <RouterLink :to="{ name: 'writingGameResult', params: { roomId: '1' } }">
-    writingGameResult</RouterLink
-  > -->
-<!--   
-  |
-  <RouterLink :to="{ name: 'runningGame', params: { roomId: '1' } }">
-    runningGame</RouterLink
-  >
-  |
-  <RouterLink :to="{ name: 'mainPage' }"> mainPage</RouterLink> |
-  <RouterLink :to="{ name: 'myPage' }">myPage</RouterLink> |
-  <RouterLink :to="{ name: 'myPageModify' }">mypageModify</RouterLink> |
-
-  <RouterLink :to="{ name: 'groupList' }">groupList</RouterLink> |
-  
-  <RouterLink
-    :to="{ name: 'runningGameResult', params: { roomId: '1' } }"
-    >runningGameResult</RouterLink
-  >
-  | -->
-
   <v-app class="bg-amber-lighten-3 gamja-flower-regular">
     <RouterView></RouterView>
   </v-app>
